9e3e216ba78b8413da0b5b599ee912757cfbf485
Hilton Santos <jhilton930@gmail.com>
2020-05-28 22:17:18 -0300

    Update Aula Hasura

--- aula_api_rest/test/widget_test.dart
+++ aula_api_rest/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:aula_hasura/main.dart';
+import 'package:aula_api_rest/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
