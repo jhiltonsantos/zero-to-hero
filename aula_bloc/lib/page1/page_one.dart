79fdc6ff08b3305aea9ddd6df6b4c829cde75269
Hilton Santos <jhilton930@gmail.com>
2020-07-09 23:33:42 -0300

    Add Aula06

---
+++ aula_bloc/lib/page1/page_one.dart
@@ -0,0 +1,68 @@
+import 'package:aula_bloc/shared/widgets/card_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+
+class Data {
+  final String text;
+  bool selected;
+
+  Data(this.text, {this.selected = false});
+}
+
+class OnePage extends StatefulWidget {
+  OnePage({Key key}) : super(key: key);
+
+  @override
+  _OnePageState createState() => _OnePageState();
+}
+
+class _OnePageState extends State<OnePage> {
+  var listObservable = BehaviorSubject<List<Data>>.seeded([]);
+
+  @override
+  void dispose() {
+    listObservable.close();
+    super.dispose();
+  }
+
+  void addItem() {
+    var list = listObservable.value;
+    list.add(Data('Item ${list.length}'));
+    listObservable.sink.add(list);
+  }
+
+  void selectedItem(int index) {
+    var list = listObservable.value;
+    list[index].selected = list[index].selected;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: listObservable,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return CardTile(
+                    data: snapshot.data[index],
+                    onTap: () {
+                      snapshot.data[index].selected =
+                          !snapshot.data[index].selected;
+                    },
+                  );
+                });
+          } else {
+            return Container();
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addItem,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ aula_bloc/lib/shared/widgets/card_tile.dart
@@ -0,0 +1,31 @@
+import 'package:aula_bloc/page1/page_one.dart';
+import 'package:flutter/material.dart';
+
+class CardTile extends StatefulWidget {
+  final Data data;
+  final Function onTap;
+
+  CardTile({Key key, this.data, this.onTap}) : super(key: key);
+
+  @override
+  _CardTileState createState() => _CardTileState();
+}
+
+class _CardTileState extends State<CardTile> {
+  var _selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: _selected ? Colors.green : Colors.red,
+      child: ListTile(
+        title: Text(widget.data.text),
+        onTap: () {
+          _selected = !_selected;
+          setState(() {});
+          widget.onTap();
+        },
+      ),
+    );
+  }
+}
