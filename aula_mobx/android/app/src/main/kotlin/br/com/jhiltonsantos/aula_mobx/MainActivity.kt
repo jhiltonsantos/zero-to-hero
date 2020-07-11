8413efcd0f05bfc7ef27cfcbff566053b2837b0e
Hilton Santos <jhilton930@gmail.com>
2020-07-10 23:12:49 -0300

    Add aula07

---
+++ aula_mobx/android/app/src/main/kotlin/br/com/jhiltonsantos/aula_mobx/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.jhiltonsantos.aula_mobx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aula_mobx/lib/shared/widgets/card_tile_two.dart
@@ -0,0 +1,32 @@
+
+import 'package:aula_mobx/page1/page_one.dart';
+import 'package:flutter/material.dart';
+
+class CardTileTwo extends StatefulWidget {
+  final String title;
+  final Data data;
+  final Function onTap;
+
+  CardTileTwo({Key key, this.title, this.data, this.onTap}) : super(key: key);
+
+  @override
+  _CardTileTwoState createState() => _CardTileTwoState();
+}
+
+class _CardTileTwoState extends State<CardTileTwo> {
+  var _selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: _selected ? Colors.green : Colors.redAccent,
+      child: ListTile(
+        title: Text(widget.title),
+        onTap: () {
+          _selected = !_selected;
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
