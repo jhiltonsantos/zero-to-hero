1a0db5b890937582eb084cf5c37174482dccbe1e
Hilton Santos <jhilton930@gmail.com>
2020-07-11 00:23:56 -0300

    add mobx_2

--- mobx_1/lib/controller.dart
+++ mobx_1/lib/controller.dart
@@ -1,21 +1,37 @@
-import 'package:mobx/mobx.dart' as mobx;
+import 'package:mobx/mobx.dart';
+part 'controller.g.dart';
 
-class Controller {
-  var _counter = mobx.Observable(0);
-  mobx.Action increment;
+class Controller = ControllerBase with _$Controller;
 
-  int get counter => _counter.value;
+// Gerando Código Automático
+abstract class ControllerBase with Store{
+  @observable
+  int counter = 0;
 
-  set counter(int value) => _counter.value = value;
-
-  Controller() {
-    increment = mobx.Action(_increment);
-    mobx.autorun((_) {
-      print(counter);
-    });
-  }
-
-  _increment() {
+  @action
+  increment() {
     counter++;
   }
 }
+
+
+// Gerando Código Manual
+//class Controller{
+//  var _counter = mobx.Observable(0);
+//  mobx.Action increment;
+//
+//  int get counter => _counter.value;
+//
+//  set counter(int value) => _counter.value = value;
+//
+//  Controller() {
+//    increment = mobx.Action(_increment);
+//    mobx.autorun((_) {
+//      print(counter);
+//    });
+//  }
+//
+//  _increment() {
+//    counter++;
+//  }
+//}
